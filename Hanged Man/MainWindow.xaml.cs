17001a1a87258f407a5aa9c32879605cb56a2930
muhamet kasal <muhamet.kasal@saintmichelannecy.fr>
2024-10-14 11:49:20 +0100

    MEGA Amelioration Avec L'aide de Leo^tm

--- Hanged Man/MainWindow.xaml.cs
+++ Hanged Man/MainWindow.xaml.cs
@@ -20,30 +20,75 @@ namespace Hanged_Man
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string GuessLetter;
+        private int vies = 5;
+        private string randomWord;
+
         public MainWindow()
         {
             InitializeComponent();
+            setupGame();
+        }
+
+        public TextBox HiddenWordTextBox { get; private set; }
+
+        private void setupGame()
+        {
+            List<string> list = new List<string> { "vache", "chien", "guepe", "koala", "lapin", "zebre", "ecran", "canon", "epave", "pomme" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, list.Count);
+            randomWord = list[randomIndex].ToUpper();
+            int wordLength = randomWord.Length;
+            string hiddenWord = new string('*', wordLength);
+            DisplayHiddenWord(hiddenWord);
+        }
+
+        public void runGame()
+        {
+            if (GuessLetter != null)
+            {
+                if (randomWord.Contains(GuessLetter.ToUpper()))
+                {
+                    for (int i = 0; i < randomWord.Length; i++)
+                    {
+                        if (randomWord[i].ToString().ToUpper() == GuessLetter.ToUpper())
+                        {
+                            if (Cipher != null)
+                            {
+                                Cipher.Text = Cipher.Text.Remove(i, 1).Insert(i, GuessLetter.ToUpper());
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    vies--;
+                }
+
+                if (vies == 0)
+                {
+                    MessageBox.Show("Défaite !");
+                }
+
+                if (Cipher != null && !Cipher.Text.Contains("*"))
+                {
+                    MessageBox.Show("Victoire !");
+                }
+            }
         }
 
         private void BTN_alpha_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            String btnContent = btn.Content.ToString();
+            string btnContent = btn.Content.ToString();
             btn.IsEnabled = false;
-            //Votre function 
+            GuessLetter = btn.Content.ToString();
+            runGame();
         }
 
-        static Random rnd = new Random();
-        int word = rnd.Next(0, 11);
-
-        public void display(string score)
+        private void DisplayHiddenWord(string hiddenWord)
         {
-           Life.Text = "5";
-           Life.Text = score;
-            if ( IsEnabled == false) ;
-            {
-             
-            }
+            Cipher.Text = hiddenWord;
         }
     }
 }
